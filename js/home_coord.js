88f1cc6739ac8d99c6ef85affcca15d8d16163b4
Juan Camilo <prjchec.jcardona@umanizales.edu.co>
2017-09-14 11:09:33 -0500

    calendario con eventos cargados sin interaccion

--- js/home_coord.js
+++ js/home_coord.js
@@ -1,6 +1,13 @@
 $(function(){
 
     $('#calendar').fullCalendar({
-        events: 'js/test_events.json'
+        events: {
+            url: 'http://localhost/programacion_GE/server/testEvents.php',
+            type: 'POST', // Send post data
+            error: function(err) {
+                alert('There was an error while fetching events.');
+                console.log(err)
+            }
+        }
     })
 })
--- server/testEvents.php
+++ server/testEvents.php
@@ -1,2 +1,5 @@
 <?php
 
+$json = file_get_contents('./test_events.json');
+//echo json_encode($json);
+echo $json;
