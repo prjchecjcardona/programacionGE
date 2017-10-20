3468f42f85895e040b18f91755352d037a78f2f5
Daniel Cardona <prjchec.dcardona@umanizales.edu.co>
2017-10-20 14:42:13 -0500

    last dcardona

---
+++ js/prueba.js
@@ -0,0 +1,12 @@
+zingchart.render({
+   id: 'myChart',
+   data: {
+     type: 'line',
+     
+     series: [{
+       values: [54,23,34,23,43],
+     }, {
+       values: [10,15,16,20,40]
+     }]
+   }
+ });
