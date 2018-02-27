8dad9db099f7a8b16ff884b8561d9a6ad41d5722
Juan Camilo <prjchec.jcardona@umanizales.edu.co>
2018-02-27 10:01:23 -0500

    bd to prod4

--- php/welcome_Gestora.php
+++ php/welcome_Gestora.php
@@ -37,7 +37,7 @@ function traerNombreGestora(){
 			}
 			else
 			{
-				print_r($conexion->getPDO()->errorInfo());
+				print_r($con->getPDO()->errorInfo());
 				$data['mensaje']="No se realizo la consulta";
 				$data['error']=1;
 			}
