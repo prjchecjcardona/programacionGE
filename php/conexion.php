cffdf6f175ce7ea09f61e397c5cb98855c970e7c
Sebastian Villegas <sebastian.norena@umanizales.edu.co>
2018-05-04 08:33:04 -0500

    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -3,17 +3,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-/* //DEV
+//DEV
 $database = "d4asqdqb9dlt9p";
 $uid = "ntafkvnrqqlbig";
 $pwd = "300113b0978731b5003f9916b2684ec44d7eafdeb2f3a36dca99bfcd115f33f1";
-$host = "ec2-54-197-233-123.compute-1.amazonaws.com"; */
+$host = "ec2-54-197-233-123.compute-1.amazonaws.com";
 
-//PRODUCCION
+/* //PRODUCCION
 $database = "gestjjlg_gestion_educativa";
 $uid = "gestjjlg_usr_gestion";
 $pwd = "r!Hh7XNv22E(";
-$host = "127.0.0.1"; 
+$host = "127.0.0.1";  */
 
 
 //pgsql:host=$host;port=5432;dbname=$database;user=$uid;password=$pwd
