8894fd973c139b3f98c4a3642910a18773850665
sebastian-vn <sebastian.norena@umanizales.edu.co>
2018-06-27 15:47:58 -0500

    update geoapp

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
-$(document).ready(function(){
-    getwelcome();
-    setTimeout(getWelcomeimg, 1000);
-})
+$(document).ready(function() {
+  getwelcome1();
+  setTimeout(getWelcomeimg, 1000);
+});
 
-function getwelcome(){
-    document.getElementById('welcome').setAttribute('style', 'display:flex');
-    $('#welcome').addClass('animated bounceInLeft');
+function getwelcome1() {
+  document.getElementById("welcome1").setAttribute("style", "display:flex");
+  $("#welcome1").addClass("animated bounceInLeft");
 }
 
-function getWelcomeimg(){
-    document.getElementById('welcome_img').setAttribute('style', 'display:inline');
-    $('#welcome_img').addClass('animated bounceIn');
+function outwelcome1() {
+  $("#welcome1").addClass("animated bounceOutRight");
+  document.getElementById("welcome1").setAttribute("style", "display:none");
+  getwelcome2();
+}
+
+function getwelcome2() {
+  
+  document.getElementById("welcome2").setAttribute("style", "display:flex");
+  $("#welcome2").addClass("animated bounceInLeft");
+}
+
+function getWelcomeimg() {
+  document
+    .getElementById("welcome_img")
+    .setAttribute("style", "display:inline");
+  $("#welcome_img").addClass("animated bounceIn");
 }
