b3e6149a535a92ee4c85c4a3c3c71ebd410ceed2
sebastian-vn <sebastian.norena@umanizales.edu.co>
2018-08-21 18:19:27 -0500

    banco v2

---
+++ banco/js/herramientas.js
@@ -0,0 +1,209 @@
+$(function() {
+  $(document).foundation();
+  getCompetencias();
+  getZonas();
+  getIndicadores();
+  getFicheros();
+  
+  /* ONCLICKS */
+  $(".a_download").click(function(){
+    var url = window.location.hash.substr(1);
+    getPDF(url);
+  });
+
+});
+
+function load() {
+  $("body").addClass("animated fadeIn");
+}
+
+$('.sf').change(function(){
+  var competencia = $("#sfichero_competencia").val();
+  var tema = $("#sfichero_tema").val();
+  var zona = $("#sfichero_zona").val();
+  var indicador = $("#sfichero_indicador").val();
+
+  getFicheros(competencia, tema, zona, indicador);
+});
+
+$(".filtros_competencia").change(function() {
+  var competencia = $(this).val();
+  getTemas(competencia);
+});
+
+/* Llama funcion por cada cambio en el filtro */
+$(".fp").change(function() {
+  var competencia = $("#competencia").val();
+  var tema = $("#tema").val();
+  var zona = $("#zona").val();
+  var indicador = $("#indicador").val();
+
+  getFicheros(competencia, tema, zona, indicador);
+});
+
+function getPDF(src){
+  $('#pdf').html(
+    `<embed src="${src}" type="application/pdf" width="90%" height="90%">`
+  );
+}
+
+function getCompetencias() {
+  $(".filtros_competencia").html('<option value="0" >No filtro</option>');
+  $.ajax({
+    type: "POST",
+    url: "server/getCompetencias.php",
+    data: "",
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        var comp = element.competencia;
+        var first = comp.charAt();
+        $(".filtros_competencia").append(
+          `<option value="${element.id_competencia}"> ${first} - ${
+            element.competencia
+          } </option>`
+        );
+      });
+    }
+  });
+}
+
+function getZonas() {
+  $(".filtros_zona").html('<option value="0" >No filtro</option>');
+  $.ajax({
+    type: "POST",
+    url: "server/getZonas.php",
+    data: "",
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        $(".filtros_zona").append(
+          `<option value="${element.id_zona}"> Z${element.id_zona} - ${
+            element.zonas
+          } </option>`
+        );
+      });
+    }
+  });
+}
+
+function getTemas(competencia) {
+  $(".filtros_tema").html('<option value="0" >No filtro</option>');
+  $.ajax({
+    type: "POST",
+    url: "server/getTemas.php",
+    data: {
+      competencia: competencia
+    },
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        $(".filtros_tema").append(
+          `<option value="${element.id_temas}"> ${element.id_temas} - ${
+            element.temas
+          } </option>`
+        );
+      });
+    }
+  });
+}
+
+function getIndicadores() {
+  $(".filtros_indicador").html('<option value="0" >No filtro</option>');
+  $.ajax({
+    type: "POST",
+    url: "server/getIndicadores.php",
+    data: "",
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        var ind = element.indicador;
+        var first = ind.charAt();
+        $(".filtros_indicador").append(
+          `<option value="${element.id_indicador}"> ${first} - ${
+            element.indicador
+          } </option>`
+        );
+      });
+    }
+  });
+}
+
+function getFicheros(competencia, tema, zona, indicador) {
+  $("#area-ficheros").html(`<img id="codificacion-img" src="img/codificacion.PNG" alt="">`);
+  $("#pdf").html("");
+  $.ajax({
+    type: "POST",
+    url: "server/getFicheros.php",
+    data: {
+      competencia: competencia,
+      tema: tema,
+      zona: zona,
+      indicador: indicador
+    },
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        $("#area-ficheros").append(
+          `<a class="a_download" href="#${element.fichero_url}">${element.codigo}</a>`
+        );
+      });
+
+      fichas();
+      htmlString = $("#area-ficheros").text();
+      if (htmlString.trim() == "") {
+        $("#area-ficheros").html(
+          '<p class="p-card">No hay ficheros para este filtro</p>'
+        );
+        $("#pdf").html(
+          '<p class="p-card">No hay pdfs</p>'
+        );
+      }
+    }
+  });
+}
+
+function getFicheroCodigo(competencia, tema, zona, indicador){
+  $.ajax({
+    type: "POST",
+    url: "server/getFicheros.php",
+    data: {
+      competencia: competencia,
+      tema: tema,
+      zona: zona,
+      indicador: indicador
+    },
+    dataType: "json",
+    success: function(response) {
+      response.forEach(element => {
+        $("#area-ficheros").append(
+          `<a class="a_download" href="#${element.fichero_url}">${element.codigo}</a>`
+        );
+      });
+
+      fichas();
+      htmlString = $("#area-ficheros").text();
+      if (htmlString.trim() == "") {
+        $("#area-ficheros").html(
+          '<p class="p-card">No hay ficheros para este filtro</p>'
+        );
+        $("#pdf").html(
+          '<p class="p-card">No hay pdfs</p>'
+        );
+      }
+    }
+  });
+}
+
+function fichas(){
+  $(".a_download").click(function(){
+    if($('.a_download').hasClass('selected')){
+      $('.a_download').removeClass('selected');
+      $(this).addClass('selected');
+    }else{
+      $(this).addClass('selected');
+    }
+    var url = window.location.hash.substr(1);
+    getPDF(url);
+  });
+}
---
+++ banco/server/consultas.php
@@ -0,0 +1,109 @@
+<?php
+
+function executeQuery($con, $sql)
+{
+    $result = $con->query($sql);
+    if ($result) {
+        $data = array();
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            array_push($data, $row);
+        }
+        return $data;
+    } else {
+        return $con->errorInfo()[2];
+    }
+}
+
+function getRecursosQuery($con)
+{
+    $sql = "SELECT id_archivo, rec.recurso_url || '/' || nombre_archivo AS recurso_url, nombre_archivo, rec.icon, rec.id_recurso
+  FROM archivos_recursos ar
+  JOIN recursos rec ON rec.id_recurso = ar.id_recurso";
+
+    return executeQuery($con, $sql);
+}
+
+function getFicherosQuery($con, $competencia, $tema, $zona, $indicador)
+{
+    $sql = "SELECT rec.recurso_url || '/' || zna.zonas || '/' || nombre_fichero AS fichero_url, fro.nombre_fichero, rec.icon, fro.codigo, tma.temas
+    FROM ficheros as fro
+    JOIN competencias as cpte ON fro.id_competencia = cpte.id_competencia
+    JOIN temas as tma ON tma.id_temas = fro.id_tema
+    JOIN zonas as zna ON zna.id_zona = fro.id_zona
+    JOIN recursos rec ON rec.id_recurso = fro.id_recurso";
+    /* Validar que la consulta termine en un WHERE y no en un AND */
+    $is_where = true;
+
+    if (!is_null($competencia)) {
+        if ($is_where) {
+            $sql .= " WHERE fro.id_competencia = $competencia";
+            $is_where = false;
+        } else {
+            $sql .= " AND fro.id_competencia = $competencia";
+        }
+    }
+    if (!is_null($tema)) {
+        if ($is_where) {
+            $sql .= " WHERE fro.id_tema = $tema";
+            $is_where = false;
+        } else {
+            $sql .= " AND fro.id_tema = $tema";
+        }
+    }
+    if (!is_null($zona)) {
+        if ($is_where) {
+            $sql .= " WHERE fro.id_zona = $zona";
+            $is_where = false;
+        } else {
+            $sql .= " AND fro.id_zona = $zona";
+        }
+    }
+    if (!is_null($indicador)) {
+        if ($is_where) {
+            $sql .= " WHERE id_indicador = $indicador";
+            $is_where = false;
+        } else {
+            $sql .= " AND id_indicador = $indicador";
+        }
+    }
+
+    return executeQuery($con, $sql);
+}
+
+function getCompetenciasQuery($con)
+{
+    $sql = "SELECT id_competencia, competencia
+  FROM competencias";
+
+    return executeQuery($con, $sql);
+}
+
+function getTemasQuery($con, $competencia)
+{
+    $sql = "SELECT id_temas, temas
+  FROM temas
+  WHERE compe_por_compo_compe_id_compe = $competencia";
+
+    return executeQuery($con, $sql);
+}
+
+function getZonasQuery($con)
+{
+    $sql = "SELECT id_zona, zonas
+  FROM zonas";
+
+    return executeQuery($con, $sql);
+}
+
+function getIndicadoresQuery($con)
+{
+    $sql = "SELECT id_indicador, indicador FROM indicador_fichero";
+
+    return executeQuery($con, $sql);
+}
+
+function getListaRecursosQuery($con){
+    $sql = "SELECT id_recurso, recurso, recurso_url FROM recursos";
+
+    return executeQuery($con, $sql);
+}
