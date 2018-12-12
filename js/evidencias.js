9ee051d5a2d5382415cf7c8d80c91e785fa2f578
Sebastian Villegas <sebastian.norena@umanizales.edu.co>
2018-12-12 04:35:13 -0500

    update

--- js/evidencias.js
+++ js/evidencias.js
@@ -3,41 +3,34 @@ $(function() {});
 var uploadEF = new FileUploadWithPreview("evidenciasFotograficas");
 var uploadAsis = new FileUploadWithPreview("evidenciasAsistencia");
 
-var evidencias = function getArray() {
-  var evidencias_fotograficas = [];
-  var asistencias = [];
+function uploadAsistencias() {
+  var form = new FormData();
 
   /* Ciclo de evidencias fotograficas */
-  for (let i = 0; i < uploadEF.cachedFileArray.length; i++) {
+  for (i = 0; i < uploadEF.cachedFileArray.length; i++) {
     element = uploadEF.cachedFileArray[i];
-    evidencias_fotograficas.push(element);
+    console.log(element);
+    form.append('evidencias[]', element);
   }
 
   /* Ciclo de asistencias */
-  for (let i = 0; i < uploadAsis.cachedFileArray.length; i++) {
+  for (i = 0; i < uploadAsis.cachedFileArray.length; i++) {
     element = uploadAsis.cachedFileArray[i];
-    asistencias.push(element);
+    console.log(element);
+    form.append('asistencias[]', element);
   }
 
-  return {
-    ef : evidencias_fotograficas,
-    asis : asistencias
-  }
-}
-
-function uploadAsistencias() {
-  var evidencia = new evidencias();
-
-  var formData = new FormData();
-  formData.append('ef', evidencia.ef);
-  formData.append('asis', evidencia.asis);
   $.ajax({
     type: "POST",
     url: "server/uploadRegistros.php",
-    data: formData,
+    data: form,
     dataType: "json",
     processData: false,
     contentType: false,
     success: function(response) {}
   });
+}
+
+function validarRegistros(){
+  
 }
--- server/uploadRegistros.php
+++ server/uploadRegistros.php
@@ -1,10 +1,24 @@
 <?php
 
-if(isset($_POST['asis']) && isset($_POST['ef'])){
-  $asistencias = $_POST['asis'];
-  $evidenciasFot = $_POST['ef'];
+if (isset($_POST)) {
+    if (isset($_FILES['asistencias']) && isset($_FILES['evidencias'])) {
+        $asistencias = $_FILES['asistencias'];
+        $evidenciasFot = $_FILES['evidencias'];
+    }
 
-  print_r($_POST['asis']);
-  echo '<br>';
-  print_r($_POST['ef']);
+    /* Asistencias */
+    $num_asis = count($asistencias['name']);
+
+    for ($i = 0; $i < $num_asis; $i++) {
+        if (file_exists('../registros/asistencias/' . $asistencias['name'][$i])) {
+            $json = "El archivo ya existe";
+        } else {
+            move_uploaded_file($asistencias['tmp_name'][$i], '../registros/asistencias/' . $asistencias['name'][$i]);
+            $json = "Cargado con exito";
+        }
+    }
+
+    /* Evidencias Fotograficas */
 }
+
+echo json_encode($json);
