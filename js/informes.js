83529c8697f5da914e1d5c03844a7c16c47a730f
Sebastian Villegas <sebastian.norena@umanizales.edu.co>
2018-12-19 04:47:48 -0500

    update

--- js/informes.js
+++ js/informes.js
@@ -1,4 +1,5 @@
 $(function() {
+  $('select').multipleSelect();
   getInformes();
 });
 
@@ -6,7 +7,9 @@ function getInformes() {
   $.ajax({
     type: "POST",
     url: "server/getInformes.php",
-    data: "",
+    data: {
+      informe : ""
+    },
     dataType: "json",
     success: function(response) {
       var myChart = Highcharts.chart("chart1", {
@@ -44,9 +47,70 @@ function getMunicipiosCobertura(){
   });
 }
 
+function getZonasCobertura(){
+  $.ajax({
+    type: "POST",
+    url: "server/getInformes.php",
+    data: {
+      zonas : ""
+    },
+    dataType: "json",
+    success: function (response) {
+
+    }
+  });
+}
+
+function getComportamientosCobertura(){
+  $.ajax({
+    type: "POST",
+    url: "server/getInformes.php",
+    data: {
+      comportamientos : ""
+    },
+    dataType: "json",
+    success: function (response) {
+
+    }
+  });
+}
+
+
+function getEstrategiasCobertura(){
+  $.ajax({
+    type: "POST",
+    url: "server/getInformes.php",
+    data: {
+      comportamientos : ""
+    },
+    dataType: "json",
+    success: function (response) {
+
+    }
+  });
+}
+
+function getTacticosCobertura(){
+  $.ajax({
+    type: "POST",
+    url: "server/getInformes.php",
+    data: {
+      comportamientos : ""
+    },
+    dataType: "json",
+    success: function (response) {
+
+    }
+  });
+}
+
+function getValues(){
+
+}
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "400px";
-  document.getElementsByClassName("main")[0].style.marginLeft = "400px";
+  document.getElementById("mySidenav").style.width = "450px";
+  document.getElementsByClassName("main")[0].style.marginLeft = "450px";
 }
 
 function closeNav() {
